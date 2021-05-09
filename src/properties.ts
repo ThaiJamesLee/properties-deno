783d107f2b39d403a45ef78aea6cf9832d326518
Duc Tai Ly <ductaily@yahoo.de>
2021-05-09 15:02:03 +0200

    refactor functions and move test to tests dir (#7)
    * refactor functions and move test to tests dir
    
    * fix paths
    
    * remove ignore tests
    
    * Rename test and error to camelCase

--- src/properties.ts
+++ src/properties.ts
@@ -1,12 +1,15 @@
-import { DuplicateKeyError, InvalidPropertyKeyError, NotAPropertiesFileError } from "./properties_error.ts";
-
-const propertiesFileEnding = ".properties";
-
+import { DuplicateKeyError, InvalidPropertyKeyError, NotAPropertiesFileError } from "./propertiesError.ts";
+import { keyIsValid, mapToString, requiresPropertyFileEnding, sanitizeString } from "./utils.ts"
 
 class Properties {
 
-    properties: Map<string, string>;
+    public properties: Map<string, string>;
 
+    /**
+     * Constructs a property instance. 
+     * Optionally with a given Map.
+     * @param properties Optional argument. Initializes a new map if no Map is passed.
+     */
     constructor(properties?: Map<string, string>) {
         if(properties) {
             this.validateKeys(properties);
@@ -16,7 +19,11 @@ class Properties {
         }
     }
 
-    private validateKeys(map:Map<string, string>):void {
+    /**
+     * Check the keys of the given Map.
+     * @param map Map to be checked.
+     */
+    private validateKeys(map: Map<string, string>): void {
         map.forEach((key, values) => {
             if(!keyIsValid(sanitizeString(key))) {
                 throw new InvalidPropertyKeyError('Keys can not be empty or contain whitespaces!');
@@ -28,10 +35,10 @@ class Properties {
      * Writes the values of the map in a .properties file.
      * @param filePath The .properties file path where the properties should be written.
      */
-    async store(filePath: string):Promise<void> {
+    async store(filePath: string): Promise<void> {
         requiresPropertyFileEnding(filePath);
 
-        let propertiesInput = this.mapToString(this.properties);
+        const propertiesInput = mapToString(this.properties);
     
         if(propertiesInput) {
             Deno.writeTextFile(filePath, propertiesInput);
@@ -44,29 +51,31 @@ class Properties {
      * The keys can not be empty or contain whitespaces.
      * On load the old properties will be discarded.
      * 
-     * @param filePath The path of the .properties that should be loaeded.
+     * @param filePath The path of the .properties that should be loaded.
      */
-    async load(filePath: string):Promise<void> {
+    async load(filePath: string): Promise<void> {
         requiresPropertyFileEnding(filePath);
 
-        this.properties = new Map();
-
-        let decoder = new TextDecoder('utf-8');
-        let rawProperties:string = decoder.decode(await Deno.readFile(filePath));
+        const decoder = new TextDecoder('utf-8');
+        const rawProperties: string = decoder.decode(await Deno.readFile(filePath));
 
         this.parse(rawProperties);
     }
 
-    public parse(text:string):void
-    {
-        let propertyLine: string[] = text.split("\n");
+    /**
+     * Reads the properties string.
+     * Expects format: "PROPERTY_1=value1\nPROPERTY_2=value2\n..."
+     * @param text String containing properties.
+     */
+    parse(text: string): void {
+        const propertyLine: string[] = text.split("\n");
     
         propertyLine.forEach(element => {
             if(element.match(/.+=.+/)) {
-                let tokens: string[] = element.split("=");
-                let key: string = tokens[0];
-                key = sanitizeString(key);
-                let value = tokens[1];
+                // Index 0 contains key and index 1 contains value
+                const tokens: string[] = element.split("=");
+                const key: string = sanitizeString(tokens[0]);
+                const value: string = tokens[1] || ""
                 
                 if(keyIsValid(key)) {
                     if(!this.properties.has(key)) {
@@ -80,45 +89,6 @@ class Properties {
             }
         });
     }
-
-    private mapToString(input:Map<string, string>):string {
-        let result: string = "";
-
-        input.forEach((value, key) => {
-            result += `${key}=${value}\n`;
-        });
-
-        return result;
-    }
-
 }
-
-// assume already sanitizeString method called
-function keyIsValid(key: string):boolean {
-    if(key) {
-        if(key.match(/\s+/)) {
-            return false;
-        }
-        return true;
-    }
-    return false;
-}
-
-function sanitizeString(s: string):string {
-    if(s.startsWith(" ")) {
-        s = s.trimLeft();
-    } 
-    if(s.endsWith(" ")) {
-        s = s.trimRight();
-    }
-    return s;
-}
-
-function requiresPropertyFileEnding(path: string): void {
-    if(!path.endsWith(propertiesFileEnding)) {
-        throw new NotAPropertiesFileError("A properties file must have the file-ending of .properties!");
-    }
-}
-
 
 export { Properties }
--- tests/properties.test.ts
+++ tests/properties.test.ts
@@ -1,29 +1,33 @@
-import { Properties } from "./properties.ts";
-import { DuplicateKeyError, InvalidPropertyKeyError, NotAPropertiesFileError } from "./properties_error.ts";
-import { assertThrowsAsync, assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts"
+import { Properties } from "../src/properties.ts";
+import { DuplicateKeyError, InvalidPropertyKeyError, NotAPropertiesFileError } from "../src/propertiesError.ts";
+import { assertThrowsAsync, assertEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts";
  
-const props = new Properties();
+const __dirname = './'
+
+console.log(__dirname)
 
 const testfiles = [
     {
-        file: "./testfile.properties",
+        file: __dirname + "testfile.properties",
         content: "A_KEY=A test key.\nA_SECOND_KEY =A second test key.\nA_THIRD_KEY = A third test key.\n\nA_FOURTH_KEY= A fourth test key.        \n"
     },
     {
-        file: "./testfile2.properties",
+        file: __dirname + "testfile2.properties",
         content: " = a test value\n= test value 2\nA Key = Test Value 3"
     },
     {
-        file: "./testfile3.properties",
+        file: __dirname + "testfile3.properties",
         content: "A_KEY=A test key.\nA_SECOND_KEY =A second test key.\nA_THIRD_KEY = A third test key.\n\n\nA_FOURTH_KEY= A fourth test key.\nA_FIFTH_KEY =a fifth test key\nA_SECOND_KEY =the second test key a second time."
     }
 ]
 
 Deno.test("Assert Throws InvalidPropertyKeyError", async function (): Promise<void> {
+    const props = new Properties();
+
     await assertThrowsAsync(
         async (): Promise<void> => {
             await Deno.writeTextFile(testfiles[1].file, testfiles[1].content);
-            await props.load("./testfile2.properties");
+            await props.load(testfiles[1].file);
         },
         InvalidPropertyKeyError,
         "Keys can not be empty or contain whitespaces!",
@@ -39,11 +43,13 @@ Deno.test("Assert Throws InvalidPropertyKeyError", async function (): Promise<vo
         InvalidPropertyKeyError,
         "Keys can not be empty or contain whitespaces!",
     );
-    await Deno.remove("./testfile2.properties");
+    await Deno.remove(testfiles[1].file);
 });
 
 
 Deno.test("Assert Throws NotAPropertiesFileError", async function (): Promise<void> {
+    const props = new Properties();
+
     await assertThrowsAsync(
         async (): Promise<void> => {
             await props.load("./testfile2.props");
@@ -55,29 +61,32 @@ Deno.test("Assert Throws NotAPropertiesFileError", async function (): Promise<vo
 
 
 Deno.test("Assert Throws DuplicateKeyError", async function (): Promise<void> {
+    const props = new Properties();
+
     await assertThrowsAsync(
         async (): Promise<void> => {
             await Deno.writeTextFile(testfiles[2].file, testfiles[2].content);
-            await props.load("./testfile3.properties");
+            await props.load(testfiles[2].file);
         },
         DuplicateKeyError,
         "A properties file can not contain multiple keys of the same name!",
       );
-      await Deno.remove("./testfile3.properties");
+      await Deno.remove(testfiles[2].file);
 });
 
 
 Deno.test("Assert Loading Test", async () => {
+    const properties = new Properties()
     await Deno.writeTextFile(testfiles[0].file, testfiles[0].content);
-
-    await props.load("./testfile.properties");
-    let propertyMap = props.properties;
+    
+    await properties.load(testfiles[0].file);
+    const propertyMap = properties.properties;
 
     assertEquals(propertyMap.get("A_KEY"), "A test key.");
     assertEquals(propertyMap.get("A_SECOND_KEY"), "A second test key.");
     assertEquals(propertyMap.get("A_THIRD_KEY"), "A third test key.");
     assertEquals(propertyMap.get("A_FOURTH_KEY"), "A fourth test key.");
 
-    await Deno.remove("./testfile.properties");
+    await Deno.remove(testfiles[0].file);
 });
 
